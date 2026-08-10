be729bf9e4f4af33febccfccfb3a3b95dc841d22
mhdieleven <mahimidou4@gmail.com>
2026-08-11 00:21:43 +0100

    filled site with placeholder text

--- src/componenets/info.js
+++ src/componenets/info.js
@@ -5,17 +5,24 @@ const info ={
         type:'homes',
         content:[
             {id:11 ,desc:'hello',src:'ap1', guestfav:true , isfav:true ,price:"yo left kidney" , rating:'5'},
-            {id:12, desc:'hello',src:'ap2', guestfav:false , isfav:true ,price:"yo left kidney" , rating:'5'},
-            {id:13, desc:'hello',src:'ap3', guestfav:true , isfav:true ,price:"yo left kidney" , rating:'5'},
-            {id:14, desc:'hello',src:'ap1', guestfav:true , isfav:true ,price:"yo left kidney" , rating:'5'},
+            {id:12, desc:'hello',src:'ap2', guestfav:true , isfav:true ,price:"yo left kidney" , rating:'5'},
+            {id:13, desc:'hello',src:'ap3', guestfav:false , isfav:true ,price:"yo left kidney" , rating:'5'},
+            {id:14, desc:'hello',src:'ap4', guestfav:false , isfav:true ,price:"yo left kidney" , rating:'5'},
+            {id:15 ,desc:'hello',src:'ap5', guestfav:true , isfav:true ,price:"yo left kidney" , rating:'5'},
+            {id:16, desc:'hello',src:'ap6', guestfav:false , isfav:true ,price:"yo left kidney" , rating:'5'},
+            {id:17, desc:'hello',src:'ap7', guestfav:true , isfav:true ,price:"yo left kidney" , rating:'5'},
+            {id:18, desc:'hello',src:'ap8', guestfav:true , isfav:true ,price:"yo left kidney" , rating:'5'},
         ]
     },
     12:{
         id:12,
-        description:'Popular homes in my ass',
+        description:'Popular homes in Oran',
         type:'homes',
         content:[
-            {id:11, src:null, guestfav:true , isfav:true ,price:"yo left kidney" , rating:'5'}
+            {id:11 ,desc:'hello',src:'ap5', guestfav:true , isfav:true ,price:"yo left kidney" , rating:'5'},
+            {id:12, desc:'hello',src:'ap6', guestfav:false , isfav:true ,price:"yo left kidney" , rating:'5'},
+            {id:13, desc:'hello',src:'ap7', guestfav:true , isfav:true ,price:"yo left kidney" , rating:'5'},
+            {id:14, desc:'hello',src:'ap8', guestfav:true , isfav:true ,price:"yo left kidney" , rating:'5'},
         ]
     },
 }
