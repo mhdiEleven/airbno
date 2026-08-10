e015876bb2d59d13d7d75199cad25447e9c03ace
mhdieleven <mahimidou4@gmail.com>
2026-08-11 00:09:56 +0100

    added favorite button altho the pages are yet to work

--- src/componenets/content.jsx
+++ src/componenets/content.jsx
@@ -1,26 +1,8 @@
 import st from './content.module.css'
-import { useState } from 'react'
-const info = {
-//PRODUCT ARGUMENTS ARE : desc ,src, guest , isfav , price , rating
-    11:{description:'Popular homes in Oran',
-        type:'homes',
-        content:[
-            {id:1 ,desc:'hello', src:'test', guestfav:true , isfav:true ,price:"yo left kidney" , rating:'5'},
-            {id:2, src:'test', guestfav:false , isfav:true ,price:"yo left kidney" , rating:'5'},
-            {id:3, src:'test', guestfav:true , isfav:true ,price:"yo left kidney" , rating:'5'},
-            {id:4, src:'test', guestfav:true , isfav:true ,price:"yo left kidney" , rating:'5'},
-        ]
-    },
-    12:{description:'Popular homes in my ass',
-        type:'homes',
-        content:[
-            {id:1, src:null, guestfav:true , isfav:true ,price:"yo left kidney" , rating:'5'}
-        ]
-    },
-}
-function liked(e){
-    alert('nigga you dont have favorite code written yet')
-}
+import React, { useState } from 'react'
+import info from './info.js'
+
+
 function Desc({data}){
     return(<div className={st.desc_container}>
         <h2 className={st.text}>{data}</h2>
@@ -30,19 +12,29 @@ function Desc({data}){
     </div>)
 }
 //PRODUCT ARGUMENTS ARE : src, guest , isfav , price , rating
-function Product({data}){
+function Product({data , liked , change_state}){
+    function handleLike(full_id ){
+        console.log(liked)
+        liked.includes(full_id)?
+        change_state(liked.filter(x => x !== full_id)):
+        change_state([...liked,full_id])
+    }
+    const full_id = `${data.idc}${data.id}`
     return(<div className={st.card}>
         <div className={st.img_container}>
             <img src={'/products/'+data.src+'.png'} className={st.img}/>
 
                 
             {data.guestfav && <p className={st.guest}>Guest favorite</p>}
-            {data.isfav ? (
-            <button className={st.active_container} onClick={liked}>
-                <svg className={st.active} width="40px" height="40px" viewBox="0 0 24 24" fill="hsl(0deg,0%,25%)" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M12 6.00019C10.2006 3.90317 7.19377 3.2551 4.93923 5.17534C2.68468 7.09558 2.36727 10.3061 4.13778 12.5772C5.60984 14.4654 10.0648 18.4479 11.5249 19.7369C11.6882 19.8811 11.7699 19.9532 11.8652 19.9815C11.9483 20.0062 12.0393 20.0062 12.1225 19.9815C12.2178 19.9532 12.2994 19.8811 12.4628 19.7369C13.9229 18.4479 18.3778 14.4654 19.8499 12.5772C21.6204 10.3061 21.3417 7.07538 19.0484 5.17534C16.7551 3.2753 13.7994 3.90317 12 6.00019Z" stroke="white" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
-            </button>
+            
+            {liked?.includes(full_id) ? (
+                <button className={`${st.active_container} ${st.colored}`} onClick={() =>handleLike(full_id)}>
+                    <svg className={st.active} width="40px" height="40px" viewBox="0 0 24 24" fill="hsl(0deg,50%,50%)" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M12 6.00019C10.2006 3.90317 7.19377 3.2551 4.93923 5.17534C2.68468 7.09558 2.36727 10.3061 4.13778 12.5772C5.60984 14.4654 10.0648 18.4479 11.5249 19.7369C11.6882 19.8811 11.7699 19.9532 11.8652 19.9815C11.9483 20.0062 12.0393 20.0062 12.1225 19.9815C12.2178 19.9532 12.2994 19.8811 12.4628 19.7369C13.9229 18.4479 18.3778 14.4654 19.8499 12.5772C21.6204 10.3061 21.3417 7.07538 19.0484 5.17534C16.7551 3.2753 13.7994 3.90317 12 6.00019Z" stroke="white" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+                </button>
             ) : (
-            <button className={st.active} onClick={liked}></button>
+                <button className={st.active_container} onClick={() =>handleLike(full_id)}>
+                    <svg className={st.active} width="40px" height="40px" viewBox="0 0 24 24" fill="hsl(0deg,0%,25%)" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M12 6.00019C10.2006 3.90317 7.19377 3.2551 4.93923 5.17534C2.68468 7.09558 2.36727 10.3061 4.13778 12.5772C5.60984 14.4654 10.0648 18.4479 11.5249 19.7369C11.6882 19.8811 11.7699 19.9532 11.8652 19.9815C11.9483 20.0062 12.0393 20.0062 12.1225 19.9815C12.2178 19.9532 12.2994 19.8811 12.4628 19.7369C13.9229 18.4479 18.3778 14.4654 19.8499 12.5772C21.6204 10.3061 21.3417 7.07538 19.0484 5.17534C16.7551 3.2753 13.7994 3.90317 12 6.00019Z" stroke="white" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+                </button>
             )}
 
         </div>
@@ -60,20 +52,25 @@ function Product({data}){
         </div>
     </div>)
 }
-function Content({data , fav ,setfav}){
+
+function Content({data}){
+    const [liked , change_state] = useState([])
+
     return(<div className={st.everything}>
         
-        {Object.values(info).map((inf) =>
-            <>
-                <Desc data={inf.description} key={info}/>
+        {Object.values(info).map((inf) =>{
+            const Sp_id = inf
+            return(
+            <React.Fragment key={inf.id}>
+                <Desc data={inf.description} />
                 <div className={st.wrapper}>
                     {inf.content.map(
-                        p =><Product data={p} key={p.id}></Product>
+                        p =><Product data={{...p , idc:(inf.id)}} change_state={change_state} key={p.id} liked={liked}></Product>
                     
                     )}
                 </div>
 
-            </>
+            </React.Fragment>)}
         )}
 
     </div>)
---
+++ src/componenets/info.js
@@ -0,0 +1,22 @@
+const info ={
+    11:{
+        id:11,
+        description:'Popular homes in Oran',
+        type:'homes',
+        content:[
+            {id:11 ,desc:'hello',src:'ap1', guestfav:true , isfav:true ,price:"yo left kidney" , rating:'5'},
+            {id:12, desc:'hello',src:'ap2', guestfav:false , isfav:true ,price:"yo left kidney" , rating:'5'},
+            {id:13, desc:'hello',src:'ap3', guestfav:true , isfav:true ,price:"yo left kidney" , rating:'5'},
+            {id:14, desc:'hello',src:'ap1', guestfav:true , isfav:true ,price:"yo left kidney" , rating:'5'},
+        ]
+    },
+    12:{
+        id:12,
+        description:'Popular homes in my ass',
+        type:'homes',
+        content:[
+            {id:11, src:null, guestfav:true , isfav:true ,price:"yo left kidney" , rating:'5'}
+        ]
+    },
+}
+export default info
